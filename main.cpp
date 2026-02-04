be03db58110aef36a80496d1060d3df8d3633555
map-r123 <maandamap123@gmail.com>
2026-02-04 15:44:10 +0200

    Refactor main.cpp for improved readability and flow
    Cleaned up the code and fixed some logic errors

--- main.cpp
+++ main.cpp
@@ -1,63 +1,30 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
-
 using namespace std;
 
 int main() {
-    // Write C++ code here
-    int num1=0,counter=0;
-    char runAgain;
+    int num1;
+    char runAgain = 'y';
 
-    cout << "Enter a number >1: ";
-    cin >> num1;
+    while (runAgain == 'y') {
+        int counter = 0;
 
-    do
-    {
-        if(num1%2==0)
-        {
-            num1 = num1/2;
-            cout << num1 << endl;
-            counter = counter +1;
-        }
-        else
-        {
-            num1 = num1*3+1;
-            cout << num1 << endl;
-            counter = counter +1;
-        }
-    }
-    while(!(num1==1));
-    cout << "Done. This ran " << counter << " times\n" ;
-
-    cout << "do you want to run again: ";
-    cin >> runAgain;
-
-    //while loop to run again
-    while(runAgain == 'y')
-    {
         cout << "Enter a number >1: ";
         cin >> num1;
-    do
-    {
-        if(num1%2==0)
-        {
-            num1 = num1/2;
-            cout << num1 << endl;
-            counter = counter +1;
-        }
-        else
-        {
-            num1 = num1*3+1;
-            cout << num1 << endl;
-            counter = counter +1;
-        }
-    }
-    while(!(num1==1));
 
-    cout << "do you want to run again: ";
-    cin >> runAgain;
+        while (num1 != 1) {
+            if (num1 % 2 == 0)
+                num1 /= 2;
+            else
+                num1 = num1 * 3 + 1;
+
+            cout << num1 << endl;
+            counter++;
+        }
+
+        cout << "Done. This ran " << counter << " times\n";
+        cout << "Do you want to run again (y/n): ";
+        cin >> runAgain;
     }
 
-    cout << "Done. This ran " << counter << " times";
     return 0;
 }
